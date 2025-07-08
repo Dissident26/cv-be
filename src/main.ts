cdbfb8d475d7e570e8f4e113553db7542f70137d
Artem Zhukovsky <azhukovsky@enterrasolutions.com>
2025-07-08 17:43:36 +0200

    remove swagger

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  const config = new DocumentBuilder()
-    .setTitle('CV API')
-    .setDescription('API documentation for CV backend (REST endpoints only)')
-    .setVersion('1.0')
-    .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
-
   await app.listen(3000);
 }
 bootstrap();
